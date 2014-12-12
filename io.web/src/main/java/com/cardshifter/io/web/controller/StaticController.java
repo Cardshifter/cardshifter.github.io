c2afa3f3548f792666d90aa3afb9c4c94b4b0359
marcandregirard <marcandr.girard@gmail.com>
2014-12-11 20:06:35 -0500

    Minimum app

---
+++ io.web/src/main/java/com/cardshifter/io/web/controller/StaticController.java
@@ -0,0 +1,10 @@
+package com.cardshifter.io.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value = "/")
+@Controller
+public class StaticController {
+
+}
