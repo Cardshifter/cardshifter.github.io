7956f27031910a7d9c21ea2343654f439b78c08b
Phrancis <Phrancis@users.noreply.github.com>
2015-01-30 13:10:32 -0500

    Update StaticController.java
    Added all links to left nav menu to Controller. This solves Issue #23.

--- io.web/src/main/java/com/cardshifter/io/web/controller/StaticController.java
+++ io.web/src/main/java/com/cardshifter/io/web/controller/StaticController.java
@@ -14,9 +14,34 @@ public class StaticController {
 		return "index";
 	}
 	
+	@RequestMapping(value = "/play-cardshifter",method = RequestMethod.GET)
+	public String playCardshifter(Model model) {
+		return "play-cardshifter";
+	}
+	
+	@RequestMapping(value = "/news",method = RequestMethod.GET)
+	public String news(Model model) {
+		return "news";
+	}
+	
+	@RequestMapping(value = "/project",method = RequestMethod.GET)
+	public String project(Model model) {
+		return "project";
+	}
+
 	@RequestMapping(value = "/contributors",method = RequestMethod.GET)
 	public String contributors(Model model) {
 		return "contributors";
+	}
+	
+	@RequestMapping(value = "/images",method = RequestMethod.GET)
+	public String images(Model model) {
+		return "images";
+	}
+	
+	@RequestMapping(value = "/contribute",method = RequestMethod.GET)
+	public String contribute(Model model) {
+		return "contribute";
 	}
 	
 	@RequestMapping(value = "/mods",method = RequestMethod.GET)
@@ -24,9 +49,29 @@ public class StaticController {
 		return "mods";
 	}
 	
-	@RequestMapping(value = "/contribute",method = RequestMethod.GET)
-	public String contribute(Model model) {
-		return "contribute";
+	@RequestMapping(value = "/cyborg-chronicles",method = RequestMethod.GET)
+	public String cyborgChronicles(Model model) {
+		return "cyborg-chronicles";
+	}
+	
+	@RequestMapping(value = "/new-mod",method = RequestMethod.GET)
+	public String newMod(Model model) {
+		return "new-mod";
+	}
+	
+	@RequestMapping(value = "/contact-us",method = RequestMethod.GET)
+	public String ContactUs(Model model) {
+		return "contact-us";
+	}
+	
+	@RequestMapping(value = "/report-bug",method = RequestMethod.GET)
+	public String reportBug(Model model) {
+		return "report-bug";
+	}
+	
+	@RequestMapping(value = "/feature-request",method = RequestMethod.GET)
+	public String featureRequest(Model model) {
+		return "feature-request";
 	}
 	
 }
