e95bd54442b38d29b8b9132bc95916eac670ebf6
marcandregirard <marcandr.girard@gmail.com>
2015-03-11 22:00:50 -0400

    added some tests yeah

---
+++ io.web/src/test/java/com/cardshifter/io/web/config/TestConfig.java
@@ -0,0 +1,43 @@
+package com.cardshifter.io.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "com.cardshifter.io.web" })
+public class TestConfig extends WebMvcConfigurerAdapter {
+	
+	@Bean
+	ThymeleafViewResolver viewResolver() {
+		ThymeleafViewResolver templateResolver = new ThymeleafViewResolver();
+		templateResolver.setOrder(1);
+		templateResolver.setTemplateEngine(templateEngine());
+		templateResolver.setViewNames(new String[] { "*" });
+
+		return templateResolver;
+	}
+	
+	@Bean
+	ServletContextTemplateResolver templateResolver() {
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+		templateResolver.setPrefix("/WEB-INF/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("HTML5");
+		templateResolver.setOrder(10);
+		return templateResolver;
+	}
+
+	@Bean
+	SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+		return templateEngine;
+	}
+}
---
+++ io.web/src/test/java/com/cardshifter/io/web/controller/StaticControllerTest.java
@@ -0,0 +1,60 @@
+package com.cardshifter.io.web.controller;
+
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.cardshifter.io.web.config.TestConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { TestConfig.class })
+@WebAppConfiguration
+public class StaticControllerTest {
+	
+    @Autowired
+    WebApplicationContext ctx;
+ 
+    MockMvc mvc;
+    
+    @Before 
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+    }
+    
+    @Test
+    public void testIndex() throws Exception {
+    	MvcResult result = this.mvc.perform(get("/"))
+    	.andExpect(status().isOk())
+    	.andExpect(view().name("template"))
+    	.andReturn();
+    	
+        String content = result.getResponse().getContentAsString();
+        assertNotNull(content);
+    }
+    
+    @Test
+    public void testPlayCardshifter() throws Exception {
+    	MvcResult result = this.mvc.perform(get("/play-cardshifter"))
+    	.andExpect(status().isOk())
+    	.andExpect(view().name("template"))
+    	.andReturn();
+    	
+        String content = result.getResponse().getContentAsString();
+        assertNotNull(content);
+    }
+ 
+
+}
