ad038e89633207d290528d7bb8265394fa6140ca
marcandregirard <marcandr.girard@gmail.com>
2015-03-12 17:17:17 -0400

    MOAR TESTS

--- io.web/src/test/java/com/cardshifter/io/web/controller/StaticControllerTest.java
+++ io.web/src/test/java/com/cardshifter/io/web/controller/StaticControllerTest.java
@@ -46,8 +46,85 @@ public class StaticControllerTest {
     }
     
     @Test
+    public void testNews() throws Exception {
+    	MvcResult result = this.mvc.perform(get("/news"))
+    	.andExpect(status().isOk())
+    	.andExpect(view().name("template"))
+    	.andReturn();
+    	
+        String content = result.getResponse().getContentAsString();
+        assertNotNull(content);
+    }
+    
+    @Test
+    public void testProject() throws Exception {
+    	MvcResult result = this.mvc.perform(get("/project"))
+    	.andExpect(status().isOk())
+    	.andExpect(view().name("template"))
+    	.andReturn();
+    	
+        String content = result.getResponse().getContentAsString();
+        assertNotNull(content);
+    }
+    
+    @Test
+    public void testContributors() throws Exception {
+    	MvcResult result = this.mvc.perform(get("/contributors"))
+    	.andExpect(status().isOk())
+    	.andExpect(view().name("template"))
+    	.andReturn();
+    	
+        String content = result.getResponse().getContentAsString();
+        assertNotNull(content);
+    }
+    
+    @Test
+    public void testScreenshots() throws Exception {
+    	MvcResult result = this.mvc.perform(get("/screenshots"))
+    	.andExpect(status().isOk())
+    	.andExpect(view().name("template"))
+    	.andReturn();
+    	
+        String content = result.getResponse().getContentAsString();
+        assertNotNull(content);
+    }
+    
+    @Test
+    public void testContribute() throws Exception {
+    	MvcResult result = this.mvc.perform(get("/contribute"))
+    	.andExpect(status().isOk())
+    	.andExpect(view().name("template"))
+    	.andReturn();
+    	
+        String content = result.getResponse().getContentAsString();
+        assertNotNull(content);
+    }
+    
+    @Test
+    public void testMods() throws Exception {
+    	MvcResult result = this.mvc.perform(get("/mods"))
+    	.andExpect(status().isOk())
+    	.andExpect(view().name("template"))
+    	.andReturn();
+    	
+        String content = result.getResponse().getContentAsString();
+        assertNotNull(content);
+    }
+    
+    @Test
     public void testPlayCardshifter() throws Exception {
     	MvcResult result = this.mvc.perform(get("/play-cardshifter"))
+    	.andExpect(status().isOk())
+    	.andExpect(view().name("template"))
+    	.andReturn();
+    	
+        String content = result.getResponse().getContentAsString();
+        assertNotNull(content);
+    }
+    
+    @Test
+    public void testNewMod() throws Exception {
+    	MvcResult result = this.mvc.perform(get("/new-mod"))
     	.andExpect(status().isOk())
     	.andExpect(view().name("template"))
     	.andReturn();
