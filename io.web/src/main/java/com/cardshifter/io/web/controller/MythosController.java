32f6b092d3f123c91b44c1b77b1a9a9ed0c6601d
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2015-08-25 02:15:30 -0400

    Create and rename things for MythosController.java based on the CyborgChroniclesController.java - NOT TESTED YET

---
+++ io.web/src/main/java/com/cardshifter/io/web/controller/MythosController.java
@@ -0,0 +1,44 @@
+package com.cardshifter.io.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RequestMapping(value = "/mythos")
+@Controller
+public class MythosController {
+	private static final String MYTHOS_TEMPLATE = "/mythos/template";
+	
+	// General links
+	@RequestMapping(method = RequestMethod.GET)
+	public String index(Model model) {
+		model.addAttribute("view","index");
+		return MYTHOS_TEMPLATE;
+	}
+	@RequestMapping(value = "/game-rules", method = RequestMethod.GET)
+	public String gameRules(Model model) {
+		model.addAttribute("view","game-rules");
+		return MYTHOS_TEMPLATE;
+	}
+	
+	// Cards links
+	@RequestMapping(value = "/cards", method = RequestMethod.GET)
+	public String cards(Model model) {
+		model.addAttribute("view","cards");
+		return MYTHOS_TEMPLATE;
+	}
+	
+	// Story links
+	@RequestMapping(value = "/story", method = RequestMethod.GET)
+	public String story(Model model) {
+		model.addAttribute("view","story");
+		return MYTHOS_TEMPLATE;
+
+	// template link for reference only
+		@RequestMapping(value = "/page-template", method = RequestMethod.GET)
+	public String pageTemplate(Model model) {
+		model.addAttribute("view","page-template");
+		return MYTHOS_TEMPLATE;
+	}
+}
