1b282958330d13d146b3a2e970e1d1d2c4305e62
Marc-Andre <marcandr.girard@gmail.com>
2016-01-24 15:25:21 -0500

    Change the pom to deploy to tomcat and not autorun
    disabled autorun of spring boot to publish to tomcat

--- io.web/src/main/java/com/cardshifter/io/web/Application.java
+++ io.web/src/main/java/com/cardshifter/io/web/Application.java
@@ -1,16 +1,29 @@
 package com.cardshifter.io.web;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
-public class Application {
-	
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application extends SpringBootServletInitializer {
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-    }
-	
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static Class<Application> applicationClass = Application.class;
+
 	@Bean
 	public DispatcherServletConfigurer dispatcher() {
 		return new DispatcherServletConfigurer();
